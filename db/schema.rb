21d9c15d3a7a8c3371d1be93d5ba58470c37fcd8
nossu3751 <rhosungpark@gmail.com>
2020-02-27 13:21:37 -0500

    figured out how to add several markers

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_18_183249) do
+ActiveRecord::Schema.define(version: 2020_02_18_182230) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,8 +20,6 @@ ActiveRecord::Schema.define(version: 2020_02_18_183249) do
     t.string "email"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.string "password_digest"
-    t.index ["email"], name: "index_administrators_on_email", unique: true
   end
 
   create_table "sensors", force: :cascade do |t|
