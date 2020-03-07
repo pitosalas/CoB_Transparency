224f640fe9d3941ce302a7824c4e8ce7e7ca12d5
nossu3751 <rhosungpark@gmail.com>
2020-03-07 05:18:24 -0500

    changed source of data to JSON

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,34 @@
 class HomeController < ApplicationController
     def home
+
+        #@gj = GeoJson.new(Sensor.all)
+        #@sa = GeoJson.new(@sa.to_points)
         @sensors = Sensor.all
         @study_areas = StudyArea.all
         render 'home/index'
     end
+
+    def get_studyArea
+        @study_areas = StudyArea
+        render json: @study_areas.as_json
+    end
 end
+
+#app/lib/geo_json.rb PORO
+#class GeoJson
+ #   def initialize(points)
+ #   end
+
+#    def to_geo_json
+#    end
+#end
+
+#maps.html.erb
+#<%= @gj.to_geo_json %>
+
+#class StudyArea
+   # def to_points
+    #    ... [Point, Point, Point]
+    #end
+
+
