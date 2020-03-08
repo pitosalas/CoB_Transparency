76d3d16819848651e104d2d5e2a6e90ba18f1884
nossu3751 <rhosungpark@gmail.com>
2020-03-08 00:20:04 -0500

    created hash for layerinfo

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -5,13 +5,23 @@ class HomeController < ApplicationController
         #@sa = GeoJson.new(@sa.to_points)
         @sensors = Sensor.all
         @study_areas = StudyArea.all
-        
+        @layerInfo = layerInfoBuilder
         render 'home/index'
     end
 
     def get_studyArea
         @study_areas = StudyArea
         render json: @study_areas.as_json
+    end
+
+    def layerInfoBuilder
+        layers = [
+            {"layerID" => "sensor_loc", "layerName" => "Clusters"},
+            {"layerID" => "unclustered-point", "layerName" => "Sensors"},
+            {"layerID" => "sensor_loc_count", "layerName" => "# of Sensors"},
+            {"layerID" => "neighborhood_fill", "layerName" => "Boston Neighborhoods"},
+            {"layerID" => "neighborhood_line", "layerName" => "Neighborhood Boundaries"}
+        ]   
     end
 end
 
--- app/controllers/study_areas_controller.rb
+++ app/controllers/study_areas_controller.rb
@@ -6,6 +6,7 @@ class StudyAreasController < ApplicationController
   def index
     @study_areas = StudyArea.all
     @neighborhoods = 'https://gist.githubusercontent.com/nossu3751/3a92073b0333314e360185906d56772f/raw/3e86f1b621080aa431593b334114d4c0f9479b71/neighborhoods.geojson'
+    @layerInfo = layerInfoBuilder
   end
 
   # GET /study_areas/1
@@ -72,4 +73,14 @@ class StudyAreasController < ApplicationController
     def study_area_params
       params.require(:study_area).permit(:location)
     end
+
+    def layerInfoBuilder
+      layers = [
+          {"layerID" => "sensor_loc", "layerName" => "Clusters"},
+          {"layerID" => "unclustered-point", "layerName" => "Sensors"},
+          {"layerID" => "sensor_loc_count", "layerName" => "# of Sensors"},
+          {"layerID" => "neighborhood_fill", "layerName" => "Boston Neighborhoods"},
+          {"layerID" => "neighborhood_line", "layerName" => "Neighborhood Boundaries"}
+      ]   
+  end
 end
