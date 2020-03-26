924209d1e5ed7369d56d5a9068a08ea33e9849dd
youbinaa <youbinaa@gmail.com>
2020-03-26 18:37:42 -0400

    fix error in activation controller

--- app/controllers/account_activations_controller.rb
+++ app/controllers/account_activations_controller.rb
@@ -3,8 +3,7 @@ class AccountActivationsController < ApplicationController
     def edit
       administrator = Administrator.find_by(email: params[:email])
       if administrator && !administrator.activated? && administrator.authenticated?(:activation, params[:id])
-        administrator.update_attribute(:activated,    true)
-        administrator.update_attribute(:activated_at, Time.zone.now)
+        administrator.activate
         log_in administrator
         flash[:success] = "Account activated!"
         redirect_to '/admin'
