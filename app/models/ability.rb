414058710d636b2426a00cf04f2c3454fc61628a
youbinaa <youbinaa@gmail.com>
2020-04-02 14:43:29 -0400

    check authorization for activation

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -2,7 +2,7 @@ class Ability
     include CanCan::Ability
     def initialize(user)
       can :read, :all                 # allow everyone to read everything
-      return unless user.present?
+      return unless user.activated?
       can :access, :rails_admin       # only allow admin users to access Rails Admin
       can :read, :all           # allow access to dashboard
       can :manage, :all             # allow superadmins to do anything
