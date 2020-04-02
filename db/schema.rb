254021ce0b3ef4bdf610aec481e8bc294f09ea98
youbinaa <youbinaa@gmail.com>
2020-04-01 20:39:07 -0400

    merge with master

--- db/schema.rb
+++ db/schema.rb
@@ -53,4 +53,15 @@ ActiveRecord::Schema.define(version: 2020_04_01_191021) do
     t.string "latitude"
   end
 
+  create_table "users", force: :cascade do |t|
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.string "email", null: false
+    t.string "encrypted_password", limit: 128, null: false
+    t.string "confirmation_token", limit: 128
+    t.string "remember_token", limit: 128, null: false
+    t.index ["email"], name: "index_users_on_email"
+    t.index ["remember_token"], name: "index_users_on_remember_token"
+  end
+
 end
