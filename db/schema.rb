12733cbc0188d780fd20e8c77489b12da4484784
Anyan <anyanxie@gmail.com>
2020-04-09 13:22:35 -0400

    fixed margin issue

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_02_012348) do
+ActiveRecord::Schema.define(version: 2020_04_02_003825) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -28,7 +28,7 @@ ActiveRecord::Schema.define(version: 2020_04_02_012348) do
     t.datetime "activated_at"
     t.string "reset_digest"
     t.datetime "reset_sent_at"
-    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["email"], name: "index_administrators_on_email", unique: true
   end
 
   create_table "feedbacks", force: :cascade do |t|
@@ -46,11 +46,12 @@ ActiveRecord::Schema.define(version: 2020_04_02_012348) do
     t.string "op_hrs"
     t.string "datatype"
     t.string "description"
+    t.integer "study_area_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "location"
-    t.string "longitude"
-    t.string "latitude"
+    t.float "longitude"
+    t.float "latitude"
     t.string "street"
   end
 
