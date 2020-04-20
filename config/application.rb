ce6455761918fd412d4f0fb90d465da6cbf8f6da
nossu3751 <rhosungpark@gmail.com>
2020-04-20 18:57:18 -0400

    precompilation

--- config/application.rb
+++ config/application.rb
@@ -8,6 +8,7 @@ Bundler.require(*Rails.groups)
 
 module Transparency
   class Application < Rails::Application
+    config.assets.initialize_on_precompile = false
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
     # config.assets.paths << Rails.root.join("app", "assets", "fonts")
