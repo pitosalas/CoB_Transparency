bd28cd3463bf8099d70751d453e169d526e3eadc
nossu3751 <rhosungpark@gmail.com>
2020-04-21 03:21:18 -0400

    feedback area linked to clicked object, toggle button ui changed, neighborhood title font changed

---
+++ db/migrate/20200421054804_add_topic_to_feedbacks.rb
@@ -0,0 +1,5 @@
+class AddTopicToFeedbacks < ActiveRecord::Migration[6.0]
+  def change
+    add_column :feedbacks, :topic, :string
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_16_002352) do
+ActiveRecord::Schema.define(version: 2020_04_21_054804) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -36,6 +36,7 @@ ActiveRecord::Schema.define(version: 2020_04_16_002352) do
     t.string "content"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "topic"
   end
 
   create_table "sensors", force: :cascade do |t|
